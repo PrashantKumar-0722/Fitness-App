35d077bd7257c339e902657e8134bf47764818f3
Prashant <prashantkumar2531@gmail.com>
2026-04-23 01:21:14 +0530

    Entities Created

---
+++ fitness-app/fitness-app/src/main/java/com/project/fitness_app/model/Activity.java
@@ -0,0 +1,29 @@
+package com.project.fitness_app.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Entity
+public class Activity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    @Enumerated(EnumType.STRING)
+    private ActivityType type;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "json")
+    private Map<String, Object> additionalMetices;
+
+    private Integer duration;
+    private Integer caloriesBurned;
+    private LocalDateTime startTime;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}
---
+++ fitness-app/fitness-app/src/main/java/com/project/fitness_app/model/Recommendation.java
@@ -0,0 +1,37 @@
+package com.project.fitness_app.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+public class Recommendation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String type;
+
+    @Column(length = 2000)
+    private String recommendation;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "json")
+    private List<String> improvements;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "json")
+    private List<String> suggestions;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "json")
+    private List<String> safety;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}

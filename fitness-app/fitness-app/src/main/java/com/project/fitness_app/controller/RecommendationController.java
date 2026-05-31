86992bcdf3629fdbcc1d1253d0fdd492dc10bee8
Prashant <prashantkumar2531@gmail.com>
2026-06-01 00:29:28 +0530

    Recommendation get api created

--- fitness-app/fitness-app/src/main/java/com/project/fitness_app/controller/RecommendationController.java
+++ fitness-app/fitness-app/src/main/java/com/project/fitness_app/controller/RecommendationController.java
@@ -5,10 +5,9 @@ import com.project.fitness_app.model.Recommendation;
 import com.project.fitness_app.service.RecommendationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/recommendation")
@@ -23,6 +22,24 @@ public class RecommendationController {
             ){
         Recommendation recommendation=recommendationService.generateRecommendation(request);
         return  ResponseEntity.ok(recommendation);
+
+    }
+
+    @GetMapping ("/user/{userId}")
+    public ResponseEntity<List<Recommendation>> getUserRecommendation(
+            @PathVariable String userId
+    ){
+        List<Recommendation> recommendationList=recommendationService.getUserRecommendation(userId);
+        return  ResponseEntity.ok(recommendationList);
+
+    }
+
+    @GetMapping ("/activity/{activityId}")
+    public ResponseEntity<List<Recommendation>> getActivityRecommendation(
+            @PathVariable String activityId
+    ){
+        List<Recommendation> recommendationList=recommendationService.getActivityRecommendation(activityId);
+        return  ResponseEntity.ok(recommendationList);
 
     }
 }
--- fitness-app/fitness-app/src/main/java/com/project/fitness_app/service/RecommendationService.java
+++ fitness-app/fitness-app/src/main/java/com/project/fitness_app/service/RecommendationService.java
@@ -10,6 +10,8 @@ import com.project.fitness_app.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class RecommendationService {
@@ -32,5 +34,13 @@ public class RecommendationService {
                 .safety(request.getSafety())
                 .build();
         return recommendationRepository.save(recommendation);
+    }
+
+    public List<Recommendation> getUserRecommendation(String userId) {
+        return recommendationRepository.findByUserId(userId);
+    }
+
+    public List<Recommendation> getActivityRecommendation(String activityId) {
+        return recommendationRepository.findByActivityId(activityId);
     }
 }
